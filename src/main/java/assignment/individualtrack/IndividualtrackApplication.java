786c289788b59e90e95642ec7a89649ad10a848e
Minh <minh.le@student.fontys.nl>
2024-09-16 13:34:26 +0200

    some new stuff

---
+++ src/main/java/assignment/individualtrack/IndividualtrackApplication.java
@@ -0,0 +1,13 @@
+package assignment.individualtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualtrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndividualtrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/assignment/individualtrack/IndividualtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.individualtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualtrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
